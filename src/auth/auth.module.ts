07aa538205005b338e54ce07c8c4bba9d546a3d1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-23 22:58:48 -0300

    fix: user repository deps

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from './users.repository';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
