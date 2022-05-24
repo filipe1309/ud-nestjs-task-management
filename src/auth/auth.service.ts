33ab432ad0b2407662a168e82152c8d839e5adcd
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-23 22:59:06 -0300

    feat: add sigun endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class AuthService {
   constructor(private usersRepository: UsersRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -9,4 +10,13 @@ export class UsersRepository {
     @InjectRepository(User)
     private readonly userEntityRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.userEntityRepository.create({
+      username,
+      password,
+    });
+    await this.userEntityRepository.save(user);
+  }
 }
