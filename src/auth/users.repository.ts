350a85c0b90b50d63726cd66ddaea58eff040cb5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-23 22:44:20 -0300

    feat: setting up user entity with repo

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userEntityRepository: Repository<User>,
+  ) {}
+}
