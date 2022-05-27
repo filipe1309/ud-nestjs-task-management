da422d86c3779acbcdcd916124962aa72246c322
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-26 22:49:13 -0300

    feat: add guard to tasks routes

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
